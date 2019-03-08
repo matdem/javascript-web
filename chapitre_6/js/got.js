eb57384d67237e3a39f80b9967e888cec1348fd9
matdem <math_demers@yahoo.fr>
2019-03-08 16:15:04 -0500

    Bouche forEach pour ajout des noms dans la liste déroulante.

--- chapitre_6/js/got.js
+++ chapitre_6/js/got.js
@@ -1,7 +1,7 @@
 // Exercice 2: Game of Thrones
 // Consignes:
-// 1. Insérer dans le menu déroulant existant les familles de Game of Thrones
-// 2. Lorsqu'on clique sur la famille, une liste des membres apparait sous le menu déroulant.
+// 1. Insérer dans le menu déroulant existant le nom des familles de Game of Thrones
+// 2. Lorsqu'on clique sur la famille, une liste des membres apparait sous le menu déroulant sous la forme de liste à puce.
 
 // Liste de quelques maisons de Game of Thrones. Chaque maison a un code et un nom
 var maisons = [
@@ -39,4 +39,17 @@ function getPersonnages(codeMaison) {
     }
 }
 
+// Créer une boucle forEach pour mettre le nom des maisons dans la liste déroulante
+maisons.forEach(nom) {
+  // Variable pour le nom des maisons
+  let nomMaison = maisons.nom;
 
+  // Créer un nouvel élément dans le DOM de la liste déroulante
+  let optionElt = document.createElement('option');
+  // Définitions des attributs
+  optionElt.id = nomMaison;
+  optionElt.value = maisons.code;
+  optionElt.textContent = nomMaison;
+  // Insertion dans le DOM
+  document.getElementById('maison').appendChild(optionElt);
+}
